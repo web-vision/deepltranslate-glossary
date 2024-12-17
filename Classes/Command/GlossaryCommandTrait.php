fde10625e655a15773e7555dfad2a48d68a3ce2a
mhofmann <m.hofmann@web-vision.de>
2024-12-17 13:03:06 +0100

    [TASK] Move Glossary related Commands and Services to external extension

---
+++ Classes/Command/GlossaryCommandTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebVision\Deepltranslate\Glossary\Command;
+
+use WebVision\Deepltranslate\Core\Domain\Repository\GlossaryRepository;
+use WebVision\Deepltranslate\Core\Service\DeeplGlossaryService;
+
+trait GlossaryCommandTrait
+{
+    protected DeeplGlossaryService $deeplGlossaryService;
+
+    protected GlossaryRepository $glossaryRepository;
+
+    public function injectDeeplGlossaryService(DeeplGlossaryService $deeplGlossaryService): void
+    {
+        $this->deeplGlossaryService = $deeplGlossaryService;
+    }
+
+    public function injectGlossaryRepository(GlossaryRepository $glossaryRepository): void
+    {
+        $this->glossaryRepository = $glossaryRepository;
+    }
+}
