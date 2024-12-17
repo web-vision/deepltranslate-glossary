7b2ad08c9a1f07d19e8d5b9593a7347da9f53a5b
mhofmann <m.hofmann@web-vision.de>
2024-12-17 15:25:59 +0100

    [TASK] Move glossary parts to external

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use TYPO3\CMS\Core\Cache\Backend\SimpleFileBackend;
+use WebVision\Deepltranslate\Glossary\Hooks\UpdatedGlossaryEntryTermHook;
 
 (static function (): void {
     // caching configuration
@@ -10,4 +11,8 @@ use TYPO3\CMS\Core\Cache\Backend\SimpleFileBackend;
         ??= [];
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['deepltranslate_glossary']['backend']
         ??= SimpleFileBackend::class;
+
+    // hook registration
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][]
+        = UpdatedGlossaryEntryTermHook::class;
 })();
