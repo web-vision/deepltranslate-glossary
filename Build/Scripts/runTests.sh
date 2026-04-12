7e2cbd55ad708f87c4c0059e6b20080d6802fb96
Stefan Bürk <stefan@buerk.tech>
2026-04-12 21:31:09 +0200

    [TASK] DPL-158: Remove `TYPO3 v12` from `Build/Scripts/runTests.sh`
    This change modifies `Build/Scripts/runTests.sh` and
    drops the `TYPO3 v12 support` to prevent execution of
    suits against this TYPO3 version.
    
    PHP 8.1 is also removed from supported PHP versions.
    
    Further, TYPO3 v13 is now the default version because
    it's the only available and the lowed for the next
    major version.

--- Build/Scripts/runTests.sh
+++ Build/Scripts/runTests.sh
@@ -216,15 +216,13 @@ Options:
             - 15    maintained until 2027-11-11
             - 16    maintained until 2028-11-09
 
-    -t <12|13>
+    -t <13>
         Only with -s composerInstall|composerInstallMin|composerInstallMax
         Specifies the TYPO3 CORE Version to be used
-            - 12: (default) use TYPO3 v12
-            - 12: use TYPO3 v13
+            - 13: (default) use TYPO3 v13
 
-    -p <8.1|8.2|8.3|8.4|8.5>
+    -p <8.2|8.3|8.4|8.5>
         Specifies the PHP minor version to be used
-            - 8.1: use PHP 8.1
             - 8.2: (default) use PHP 8.2
             - 8.3: use PHP 8.3
             - 8.4: use PHP 8.4
@@ -295,7 +293,7 @@ fi
 
 # Option defaults
 TEST_SUITE="unit"
-CORE_VERSION="12"
+CORE_VERSION="13"
 DBMS="sqlite"
 PHP_VERSION="8.2"
 PHP_XDEBUG_ON=0
@@ -341,7 +339,7 @@ while getopts "a:b:s:d:i:p:t:xy:o:nhu" OPT; do
             ;;
         t)
             CORE_VERSION=${OPTARG}
-            if ! [[ ${CORE_VERSION} =~ ^(12|13)$ ]]; then
+            if ! [[ ${CORE_VERSION} =~ ^(13)$ ]]; then
                 INVALID_OPTIONS+=("t ${OPTARG}")
             fi
             ;;
