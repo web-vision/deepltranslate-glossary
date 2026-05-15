d986978c7b19e8606e29beb21e0b9946b0602eb9
Markus Hofmann <m.hofmann@web-vision.de>
2026-05-15 15:51:04 +0200

    [WIP][FEATURE] Update to DeepL glossary API v3

--- Classes/Client/GlossaryAPIV3ClientInterface.php
+++ Classes/Client/GlossaryAPIV3ClientInterface.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace WebVision\Deepltranslate\Glossary\Client;
 
+use DeepL\GlossaryLanguagePair;
+use DeepL\MultilingualGlossaryDictionaryEntries;
+use DeepL\MultilingualGlossaryInfo;
 use WebVision\Deepltranslate\Core\ClientInterface as DeepltranslateCoreClientInterface;
+use WebVision\Deepltranslate\Core\Exception\ApiKeyNotSetException;
 
 /**
  * Describes required implementation for Glossary API v3 compatible client implementations.
@@ -12,4 +16,42 @@ use WebVision\Deepltranslate\Core\ClientInterface as DeepltranslateCoreClientInt
  */
 interface GlossaryAPIV3ClientInterface extends DeepltranslateCoreClientInterface
 {
+    /**
+     * @return GlossaryLanguagePair[]
+     *
+     * @throws ApiKeyNotSetException
+     */
+    public function getGlossaryLanguagePairs(): array;
+
+    /**
+     * @return MultilingualGlossaryInfo[]
+     *
+     * @throws ApiKeyNotSetException
+     */
+    public function getAllGlossaries(): array;
+
+    /**
+     * @throws ApiKeyNotSetException
+     */
+    public function getGlossary(string $glossaryId): MultilingualGlossaryInfo;
+
+    /**
+     * @param string $glossaryName
+     * @param MultilingualGlossaryDictionaryEntries[] $dictionaries
+     * @return MultilingualGlossaryInfo
+     */
+    public function createGlossary(
+        string $glossaryName,
+        array $dictionaries,
+    ): MultilingualGlossaryInfo;
+
+    /**
+     * @param non-empty-string $glossaryId
+     * @param MultilingualGlossaryDictionaryEntries[] $dictionaries
+     */
+    public function updateGlossary(
+        string $glossaryId,
+        array $dictionaries,
+        ?string $name = null,
+    ): MultilingualGlossaryInfo;
 }
